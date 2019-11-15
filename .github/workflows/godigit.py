094894478909ef769baec80b05bf0937836ca6fb
naveena1910 <57788175+naveena1910@users.noreply.github.com>
2019-11-15 12:29:13 +0530

    Add files via upload

--- .github/workflows/godigit.py
+++ .github/workflows/godigit.py
@@ -1,17 +1,5 @@
-name: CI
-
-on: [push]
-
-jobs:
-  build:
-
-    runs-on: ubuntu-latest
-
-    steps:
-    - uses: actions/checkout@v1
-    - name: Run a one-line script
-      run: echo Hello, world!
-    - name: Run a multi-line script
-      run: |
-        echo Add other actions to build,
-        echo test, and deploy your project.
+a=10
+def print_a():
+	a=5
+	print(a)
+print(a)
